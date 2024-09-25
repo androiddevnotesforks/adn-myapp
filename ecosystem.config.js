e13ea80e26062d0b97d7632290f117fe9bb1aedc
androiddevnotes <awesomedevnotes@gmail.com>
2024-09-25 01:12:02 +0100

    Set server to listen on all interfaces, add PM2 config, add nginx config, update package scripts

---
+++ ecosystem.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  apps: [{
+    name: 'myapp',
+    script: './bin/www',
+    instances: 'max',
+    autorestart: true,
+    watch: false,
+    max_memory_restart: '1G',
+    env: {
+      NODE_ENV: 'production',
+      PORT: 3000
+    }
+  }]
+};
